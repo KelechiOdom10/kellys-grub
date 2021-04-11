c2e22e6720f00d36b42efa5930a41df0ba33b7cd
kelechiodom10 <kelechiodom10@gmail.com>
2021-04-11 17:37:45 +0100

    Created a helper function to send emails for password resets

---
+++ utils/sendEmail.js
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async ({ to, subject, html }) => {
+  const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_SERVICE,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_FROM,
+    to,
+    subject,
+    html,
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+};
+
+module.exports = sendEmail;
