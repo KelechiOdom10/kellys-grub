538ca169008f9e5d6f8ecaa80ef7c5df2f66c36d
Kelechiodom10 <kelechiodom10@gmail.com>
2023-01-02 11:39:54 +0000

    Add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,21 @@
+export const isProduction = process.env.NODE_ENV === "production";
+
+export const CART_ITEM_STATUS = {
+  Processing: "Processing",
+  Shipped: "Shipped",
+  Delivered: "Delivered",
+  Cancelled: "Cancelled",
+  Not_processed: "Not processed",
+};
+
+export const REVIEW_STATUS = {
+  Rejected: "Rejected",
+  Approved: "Approved",
+  Waiting_Approval: "Waiting Approval",
+};
+
+export const EMAIL_PROVIDER = {
+  Email: "Email",
+  Google: "Google",
+  Facebook: "Facebook",
+};
