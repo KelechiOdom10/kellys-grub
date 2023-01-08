511c3ef6cc5b0d4ad2846eb374f29c7cebab6a28
Kelechiodom10 <kelechiodom10@gmail.com>
2023-01-08 12:46:16 +0000

    Test tailwind css on button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
-import { Button } from "@mantine/core";
-
 function App() {
-  return <Button>Sign up</Button>;
+  return (
+    <main className="p-4">
+      <button className="bg-brand text-white py-1.5 px-4 rounded-md">
+        Let's see
+      </button>
+    </main>
+  );
 }
 
 export default App;
