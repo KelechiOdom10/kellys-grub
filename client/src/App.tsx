872318e1de287b21d80a0e2fe9219a30902c924d
Kelechiodom10 <kelechiodom10@gmail.com>
2023-01-08 23:57:17 +0000

    Test design system components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,17 @@
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+
 function App() {
   return (
     <main className="p-4">
-      <button className="bg-brand text-white py-1.5 px-4 rounded-md">
-        Let's see
-      </button>
+      <Button>Let's see</Button>
+      <Input
+        label="Email"
+        helperText="Mad man"
+        required
+        placeholder="Enter valid Email"
+        error="ukj"
+      />
     </main>
   );
 }
