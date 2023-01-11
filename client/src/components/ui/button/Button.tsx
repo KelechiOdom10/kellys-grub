264918b9faf62cb462ceba74407be7404b0ff388
Kelechiodom10 <kelechiodom10@gmail.com>
2023-01-11 20:10:36 +0000

    Add more button stories

--- client/src/components/ui/button/Button.tsx
+++ client/src/components/ui/button/Button.tsx
@@ -3,14 +3,15 @@ import { cva } from "class-variance-authority";
 import type { VariantProps } from "class-variance-authority";
 
 const styles = cva(
-  "inline-flex items-center justify-center text-sm gap-2 font-semibold relative rounded-md transition-colors focus:outline-none focus:ring-4 focus:ring-brand-300 disabled:opacity-60 disabled:cursor-not-allowed",
+  "inline-flex items-center justify-center text-sm gap-2 font-semibold relative rounded-md transition-colors focus:outline-none  disabled:opacity-60 focus:ring-2 focus:ring-offset-1 disabled:cursor-not-allowed",
   {
     variants: {
       intent: {
-        primary: "bg-brand text-white",
-        secondary: "bg-secondary text-white",
-        dark: "bg-dark text-white",
-        ghost: "bg-white hover:bg-gray-50 text-dark border-gray-200 border",
+        primary: "bg-brand text-white focus:ring-brand-300",
+        secondary: "bg-secondary text-white focus:ring-secondary-300",
+        dark: "bg-dark text-white focus:ring-gray-500",
+        ghost:
+          "bg-white hover:bg-gray-50 text-dark border-gray-200 border focus:ring-offset-0 focus:ring-dark",
       },
       fullWidth: {
         true: "w-full",
@@ -19,9 +20,10 @@ const styles = cva(
         true: "rounded-full",
       },
       size: {
-        sm: "px-3 py-1.5 leading-4 rounded-sm" /** For backwards compatibility */,
+        sm: "px-3 py-1.5 leading-4" /** For backwards compatibility */,
         md: "h-9 px-4 py-2",
         lg: "h-10 px-5 py-2.5",
+        icon: "flex justify-center min-h-[36px] min-w-[36px]",
       },
       isLoading: {
         true: "opacity-50 cursor-wait",
