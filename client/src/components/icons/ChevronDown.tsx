c323c8f049b12cf8c6e03a41274c6c1edb47d2e2
Kelechiodom10 <kelechiodom10@gmail.com>
2023-01-22 13:23:05 +0000

    feat: Install icons used in navigation menu and footer

---
+++ client/src/components/icons/ChevronDown.tsx
@@ -0,0 +1,20 @@
+import { SVGProps } from "react";
+
+export const ChevronDown = ({ width = "18", height = "18", ...props }: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <polyline points="6 9 12 15 18 9"></polyline>
+    </svg>
+  );
+};
